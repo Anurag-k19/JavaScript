b0e82a9f76775e666183df3bbc11752f7f101f62
Anurag <141508840+Anurag-k19@users.noreply.github.com>
2024-07-09 06:46:32 +0000

    datatypes

---
+++ Test/DataTypes.js
@@ -0,0 +1,20 @@
+"use strict"  //treat all js code as newer version
+
+// alert(3+3) //we are using nodejs, not browser
+
+
+
+//variables
+
+//number --> 2 to power 53
+//bigint
+//string --> " "
+//boolean --> true/false
+// null --> standalone value
+// undefined --> value is not assigned
+// symbol --> uniqueness, mostly used 
+
+console.log(typeof 3);
+console.log(typeof "Anurag");
+console.log(typeof undefined);
+console.log(typeof null);

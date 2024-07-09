3714dfe009c0192a4b7daa15caa5feaafd983638
Anurag <141508840+Anurag-k19@users.noreply.github.com>
2024-07-09 05:53:19 +0000

    variables

---
+++ Test/Variables.js
@@ -0,0 +1,7 @@
+const AccountId ="1243"
+let AccountEmail = "Anurag@gmail.com"
+var AccountPassword = "12455346"   //we dont use var because of scope issue
+AccountCity = "Noida"
+var AccountState;
+
+console.table([AccountId,AccountEmail,AccountPassword,AccountCity,AccountState]);

774164cb887b7fc27edf1752a4fd0c9500316b1d
Anurag <141508840+Anurag-k19@users.noreply.github.com>
2024-07-17 08:17:35 +0000

    math and numbers

---
+++ Test/math_numbers.js
@@ -0,0 +1,23 @@
+const score =100
+
+
+// console.log(score.toFixed(2));
+// console.log(score.toString());
+
+
+// --------------------- MATH ---------------------------------------------
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.7));
+// console.log(Math.floor(4.7));
+// console.log(Math.ceil(4.2));
+// console.log(Math.random());
+
+// console.log(Math.floor(((Math.random())*10)+1));
+
+
+let min = 10
+let max = 30
+
+console.log(Math.floor(Math.random()*(max-min)+min))
+
